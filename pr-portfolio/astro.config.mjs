28dcc75bcbea15a2cbbb9d02fb8de2b64d729fac
PRVirkrys <pala.3633@gmail.com>
2026-09-01 16:14:24 +0200

    footer

--- pr-portfolio/astro.config.mjs
+++ pr-portfolio/astro.config.mjs
@@ -10,6 +10,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+    devToolbar: { enabled: false },
     integrations: [mdx(), sitemap(), react()],
     vite: {
         plugins: [tailwindcss()],
